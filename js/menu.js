e8672b2d388fb731db110db135846ff004085d98
AMaslarov <anm7@abv.bg>
2015-03-31 20:09:10 +0300

    small changes

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+var menu = document.getElementById('menu');
+/**
+ * Hide element
+ */
+function hide(el) {
+    el.style.display = 'none';
+}
+/**
+ * Show element
+ */
+//function show(el) {
+//    el.style.display = 'block';
+//}
+/**
+ * Show the main menu after loading all assets
+ */
+//function mainMenu() {
+//    show(menu);
+//}
+/**
+ * Click handlers for the different menu screens
+ */
+document.querySelectorAll('.play')[0].addEventListener('click', function () {
+    hide(menu);
+   
+});
